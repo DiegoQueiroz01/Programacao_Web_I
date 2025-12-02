8bed695636d68e8fa9f1c27933b0ce628a23a623
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-12-02 16:15:10 -0300

    Implementação de página de login ainda com alguns erros no JavaScript.

--- Curso/GrowDev/Modulo III/Nikel/Public/Javascript/index.js
+++ Curso/GrowDev/Modulo III/Nikel/Public/Javascript/index.js
@@ -1,11 +1,35 @@
+const myModal = new bootstrap.Modal("#register-modal");
+
 //Criar conta
 document.getElementById("create-form").addEventListener("submit", function(e){
     e.preventDefault();
     const email = document.getElementById("email-create-input").value;
     const password = document.getElementById("password-create-input").value;
 
-    if(email.length < 50){
+    if(email.length < 5){
         alert("Preencha o campo com um e-mail válido.");
         return;
     }
-})
+    if(password.length < 4){
+        alert("Preencha a senha com no mínimo 4 dígitos.");
+        return;
+    }
+    saveAcount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+    if(accountCreated){
+    myModal.hide();
+    alert("Conta criada com sucesso!");
+    }
+});
+
+function saveAccount(data){
+    if(localStorage.getItem(data.login)){
+        alert("Erro: Este e-mail já está em uso.")
+        return false;
+    }
+    localStorage.setItem(data.login, JSON.stringify(data));
+    return true;
+}
